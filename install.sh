412867b540b5eb55278058d951b4e538b3b0f59c
UnknownDemonHacker <83639690+UnknownDemonHacker@users.noreply.github.com>
2022-04-10 19:55:53 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#these are all the basic packages for atom
+#hope you like it
+
+apm install clock
+apm install color-picker
+apm install build
+apm install tabnine
+apm install minimap
+apm install linter
+apm install atom-beautify
+apm install teletype
+apm install git-plus
+apm install highlight-selected
+apm install file-icons
+apm install pigments
+apm install open-recent
+apm install jumpy
+apm install pretty-json
+apm install blame
+apm install project-manager
+apm install zentabs
+apm install emmet
+apm install autoclose-html-plus
+apm install atom-liquid-snippets
+apm install liquid-language
+apm install expose
+apm install auto-complete-paths
+apm install script
+apm install turbo-javascript
